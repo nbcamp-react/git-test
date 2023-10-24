1c5bc9f56fca43fc5bf17bd83b6d4ef3b82ee2be
scseong <goidtv@naver.com>
2023-10-24 16:27:10 +0900

    first commit

---
+++ src/search.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export function performSearch(inputValue) {
+  const searchText = inputValue.toLowerCase();
+  const $movieCards = document.querySelectorAll('.movie-card');
+
+  $movieCards.forEach((card) => {
+    const title = card.querySelector('h2').textContent.toLowerCase();
+    console.log(title, searchText);
+    if (title.includes(searchText)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
